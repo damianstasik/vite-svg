da5c3d1cbe07889811f52da0f1d7f9aa33ddd082
Damian Stasik <visualfanatic@users.noreply.github.com>
2020-06-14 21:35:59 +0200

    Add example back

---
+++ example/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" :src="logoUrl" />
+  <Logo />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import logoUrl, { VueComponent as Logo } from './assets/logo.svg'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Logo
+  },
+  setup: () => ({ logoUrl })
+}
+</script>
